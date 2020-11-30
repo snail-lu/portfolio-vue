376d194a2f6ad039fbe4f70429b2d723c18cc87b
路明 <snaillu@yeah.net>
2020-11-30 16:56:49 +0800

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home msg="Welcome to My Web Site~"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
 <style>
+html, body {
+  height: 100%;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-container class="home-container">
+    <el-main class="content">
+      <img class="avatar-img" alt="Vue logo" src="./../assets/avatar.jpg">
+      <h1>{{ msg }}</h1>
+      <ul>
+        <li><a class="link-btn" href="https://github.com/Snail-Lu" target="_blank" rel="noopener">Github</a></li>
+        <li><a class="link-btn" href="https://snail-lu.github.io" target="_blank" rel="noopener">Github Pages</a></li>
+        <li><a class="link-btn" href="https://www.cnblogs.com/snaillu/" target="_blank" rel="noopener">Blog</a></li>
+        <li><a class="link-btn" href="https://snail-lu.github.io/portfolio" target="_blank" rel="noopener">portfolio</a></li>
+      </ul>
+    </el-main>
+    <el-footer class="footer">
+      <div>
+        Copyright © <a class="link-btn" href="https://www.yes-i-do.cn">yes-i-do.cn</a>. All Rights Reserved. 备案号：<a class="link-btn" target="_blank" href="https://beian.miit.gov.cn/#/Integrated/index">皖ICP备2020019744号</a>
+      </div>
+    </el-footer>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.home-container {
+  height: 100%;
+  /* background-image: linear-gradient(to right, #eea2a2 0%, #bbc1bf 19%, #57c6e1 42%, #b49fda 79%, #7ac5d8 100%); */
+  background-image: linear-gradient(to top, #fff1eb 0%, #ace0f9 100%);
+}
+.avatar-img {
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  overflow: hidden;
+  border: 2px solid #f0dfb3;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
