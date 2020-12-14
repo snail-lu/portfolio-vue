a0d5bf847b500e6c1fc94d98073e18eb3824fda4
路明 <snaillu@yeah.net>
2020-12-14 13:40:20 +0800

    配置修改

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/home/'
-      : '/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/home'
+    : '/'
 }
