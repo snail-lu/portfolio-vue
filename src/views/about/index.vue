4e97fb4042e54dabac2d325142b0610cc51d1a81
Snail-Lu <snaillu@yeah.net>
2022-01-21 20:55:57 +0800

    about页面简单开发

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,9 +1,17 @@
 <template>
-	<div>关于我</div>
+	<div class="about-me-container">开发中...</div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.about-me-container {
+	background-color: #fff;
+	margin: 0 25%;
+	height: calc(100vh - 60px);
+	text-align: center;
+	padding-top: 200px;
+}
+</style>
