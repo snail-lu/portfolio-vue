efb45985b8d0cafed91bfa87320be8ef72e79f16
Snail-Lu <snaillu@yeah.net>
2022-01-21 20:57:06 +0800

    Updates

--- static/js/chunk-2d0ceb84.f2608dff.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ceb84"],{"613f":function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("关于我")])},u=[],l={},r=l,s=t("2877"),a=Object(s["a"])(r,c,u,!1,null,null,null);e["default"]=a.exports}}]);
