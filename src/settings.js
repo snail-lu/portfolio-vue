3676f1d4f417af3af0b8a316124bf7e926693fe5
Snail-Lu <snaillu@yeah.net>
2022-05-27 23:58:11 +0800

    store修改

--- src/settings.js
+++ src/settings.js
@@ -1,26 +1,14 @@
 module.exports = {
-	title: 'VUE项目集',
+    title: 'VUE项目集',
 
-	/**
-	 * @type {boolean} true | false
-	 * @description Whether fix the header
-	 */
-	fixedHeader: false,
-
-	/**
-	 * @type {boolean} true | false
-	 * @description Whether show the logo in sidebar
-	 */
-	sidebarLogo: false,
-
-	/**
-	 * @type {array} []
-	 * @description 导航栏菜单配置
-	 */
-	menu: [
-		{ name: '首页', path: '/home' },
-		{ name: '项目', path: '/projects' },
-		{ name: '实验室', path: '/demo' },
-		{ name: '关于', path: '/about' }
-	]
+    /**
+     * @type {array} []
+     * @description 导航栏菜单配置
+     */
+    menu: [
+        { name: '首页', path: '/home' },
+        { name: '项目', path: '/projects' },
+        { name: '实验室', path: '/demo' },
+        { name: '关于', path: '/about' }
+    ]
 }
