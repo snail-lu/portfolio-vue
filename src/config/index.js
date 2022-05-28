84299a468caa634fb061d4527729a62891c26d37
Snail-Lu <snaillu@yeah.net>
2022-05-28 22:12:08 +0800

    demo数据调整

--- src/config/index.js
+++ src/config/index.js
@@ -38,58 +38,16 @@ export default {
     // demo列表
     demoList: [
         {
-            id: 1,
-            path: 'tree-expand',
-            title: '节点树展开/收缩',
-            desc: 'Element-UI中el-tree中节点树控制节点全展开/全收缩'
+            id: 12,
+            path: 'dynamic-chart',
+            title: 'echarts动态图表',
+            desc: '基于echarts实现的动态大数据图表'
         },
         {
-            id: 2,
-            path: 'table-sort',
-            title: '表格自定义排序',
-            desc: 'Element-UI中el-table中表格自定义排序'
-        },
-        {
-            id: 3,
-            path: 'table-select',
-            title: '表格分页多选',
-            desc: 'Element-UI中el-table中表格翻页后表格行选择'
-        },
-        {
-            id: 4,
-            path: 'table-transfer',
-            title: '表格穿梭',
-            desc: 'Element-UI中el-table内容选中穿梭框简单实现'
-        },
-        {
-            id: 5,
-            path: 'phone-model',
-            title: '手机模型',
-            desc: '手机模型简单实现'
-        },
-        {
-            id: 6,
-            path: 'hotspot',
-            title: '图片上绘制热区',
-            desc: '图片上绘制热区实现'
-        },
-        {
-            id: 7,
-            path: 'large-file-upload',
-            title: '大文件上传',
-            desc: '大文件上传常用组件前端实践'
-        },
-        {
-            id: 8,
-            path: 'three-js',
-            title: 'three.js实践',
-            desc: 'Three.js实践'
-        },
-        {
-            id: 9,
-            path: 'lazy-load',
-            title: '图片懒加载',
-            desc: '基于IntersectionObserver API实现的图片懒加载'
+            id: 11,
+            path: 'photo-wall',
+            title: '照片墙',
+            desc: '可以自定义拖拽/摆放的照片墙'
         },
         {
             id: 10,
@@ -98,10 +56,58 @@ export default {
             desc: '基于masonry库实现的瀑布流布局'
         },
         {
-            id: 11,
-            path: 'photo-wall',
-            title: '照片墙',
-            desc: '可以自定义拖拽/摆放的照片墙'
+            id: 9,
+            path: 'lazy-load',
+            title: '图片懒加载',
+            desc: '基于IntersectionObserver API实现的图片懒加载'
+        },
+        {
+            id: 8,
+            path: 'three-js',
+            title: 'three.js实践',
+            desc: 'Three.js实践'
+        },
+        {
+            id: 7,
+            path: 'large-file-upload',
+            title: '大文件上传',
+            desc: '大文件上传常用组件前端实践'
+        },
+        {
+            id: 6,
+            path: 'hotspot',
+            title: '图片上绘制热区',
+            desc: '图片上绘制热区实现'
+        },
+        {
+            id: 5,
+            path: 'phone-model',
+            title: '手机模型',
+            desc: '手机模型简单实现'
+        },
+        {
+            id: 4,
+            path: 'table-transfer',
+            title: '表格穿梭',
+            desc: 'Element-UI中el-table内容选中穿梭框简单实现'
+        },
+        {
+            id: 3,
+            path: 'table-select',
+            title: '表格分页多选',
+            desc: 'Element-UI中el-table中表格翻页后表格行选择'
+        },
+        {
+            id: 2,
+            path: 'table-sort',
+            title: '表格自定义排序',
+            desc: 'Element-UI中el-table中表格自定义排序'
+        },
+        {
+            id: 1,
+            path: 'tree-expand',
+            title: '节点树展开/收缩',
+            desc: 'Element-UI中el-tree中节点树控制节点全展开/全收缩'
         }
     ]
 }
