9321726fd04dc323731a888dc62d4983fa0d6ad4
Snail-Lu <snaillu@yeah.net>
2022-05-28 22:09:48 +0800

    全屏时appMain高度撑满

--- src/components/layout/AppMain.vue
+++ src/components/layout/AppMain.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="app-main">
+    <section class="app-main" :class="{ 'app-main-fullscreen': isScreenFull }">
         <transition name="fade-transform" mode="out-in">
             <router-view :key="key" />
         </transition>
@@ -7,9 +7,11 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
     name: 'AppMain',
     computed: {
+        ...mapState('settings', ['isScreenFull']),
         key() {
             return this.$route.path
         }
@@ -28,6 +30,10 @@ export default {
     box-sizing: border-box;
     margin: 0 auto;
     /* padding: 0 10px; */
+}
+
+.app-main-fullscreen {
+    height: 100vh;
 }
 </style>
 
