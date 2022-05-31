a025f657bf62d85bf6f3e857ad18fe5dfe54df6a
Snail-Lu <snaillu@yeah.net>
2022-05-31 23:24:41 +0800

    动态图表部分功能实现

---
+++ src/views/demo/dynamicChart/components/Panel.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="panel-box">
+        <div class="title">{{ title }}</div>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: ''
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.panel-box {
+    .title {
+        color: #fff;
+        font-size: 14px;
+    }
+    .content {
+        width: 200px;
+        height: 200px;
+        color: #5f8bb5;
+        background-color: rgba(95, 139, 181, 0.5);
+    }
+}
+</style>
---
+++ src/views/demo/dynamicChart/index.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="dynamic-charts-container">
+        <div class="title">
+            实时销售数据可视化平台
+            <div class="date-time">{{ time }}</div>
+        </div>
+        <div class="content flex-box">
+            <div>
+                <panel title="全国销售额" />
+                <panel title="省市销售额排名" />
+            </div>
+            <div class="flex-item-1">
+                <panel />
+                <panel title="销售额统计" />
+            </div>
+            <div>
+                <panel title="全国顾客实时总数" />
+                <panel title="店铺库存" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { getNowTime } from '@/utils/date'
+import Panel from './components/Panel.vue'
+export default {
+    components: {
+        Panel
+    },
+    data() {
+        return {
+            time: ''
+        }
+    },
+    created() {
+        this.initTime()
+    },
+    methods: {
+        // 初始化时间
+        initTime() {
+            let timer = setInterval(() => {
+                this.time = getNowTime()
+            }, 200)
+
+            // 清理定时器
+            this.$once('hook:beforeDestroy', () => {
+                clearInterval(timer)
+                timer = null
+            })
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.dynamic-charts-container {
+    height: 100vh;
+    background: url('../../../assets/images/charts_bg.jpeg') top center no-repeat;
+}
+.title {
+    background: url('../../../assets/images/charts_nav.png') top center no-repeat;
+    text-align: center;
+    height: 80px;
+    line-height: 80px;
+    font-size: 20px;
+    color: #fff;
+    position: relative;
+
+    .date-time {
+        position: absolute;
+        top: 10px;
+        right: 20px;
+        line-height: 1;
+        font-size: 16px;
+    }
+}
+</style>
