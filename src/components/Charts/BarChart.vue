4de33f8c3825c4e7d0d426f0de2b1e5dce0c6ace
Snail-Lu <snaillu@yeah.net>
2022-06-05 17:42:25 +0800

    新增柱状图

---
+++ src/components/Charts/BarChart.vue
@@ -0,0 +1,54 @@
+<template>
+    <div :style="{ width, height }"></div>
+</template>
+
+<script>
+import * as echarts from 'echarts/core'
+import { GridComponent } from 'echarts/components'
+import { BarChart } from 'echarts/charts'
+import { CanvasRenderer } from 'echarts/renderers'
+
+echarts.use([GridComponent, BarChart, CanvasRenderer])
+export default {
+    props: {
+        width: {
+            type: String,
+            default: '100%'
+        },
+        height: {
+            type: String,
+            default: '300px'
+        }
+    },
+    mounted() {
+        this.initCharts()
+    },
+    methods: {
+        initCharts() {
+            this.chart = echarts.init(this.$el)
+            this.setOptions()
+        },
+        setOptions() {
+            const option = {
+                xAxis: {
+                    type: 'category',
+                    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        data: [120, 200, 150, 80, 70, 110, 130],
+                        type: 'bar'
+                    }
+                ]
+            }
+
+            this.chart.setOption(option)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
