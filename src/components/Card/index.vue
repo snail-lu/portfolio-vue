0372b49ab36c2426eb5d5e320e9b3f5558ffe74c
Snail-Lu <snaillu@yeah.net>
2022-06-10 23:40:48 +0800

    list卡片增加插图光影加载效果，插图加载失败使用笑脸占位图

--- src/components/Card/index.vue
+++ src/components/Card/index.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="card flex-box-column flex-h-between" @click="pushUrl">
-        <div :class="`header bg-color${data.id}`">
-            <img class="cover" :src="data.coverUrl" />
+        <div :class="`header bg-color${data.id} ${isLoading ? 'animated-bg' : ''}`">
+            <img class="cover" :src="data.coverUrl" @load="load" @error="error" v-if="!isError" />
+            <div class="placeholder-cover" v-else>
+                <img class="placeholder-cover1" src="@/assets/icons/smile.svg" />
+                <img class="placeholder-cover2" src="@/assets/icons/wink.svg" />
+            </div>
         </div>
         <div class="card-info">
             <div class="card-info-title">{{ data.title }}</div>
@@ -17,6 +21,12 @@ export default {
             type: Object
         }
     },
+    data() {
+        return {
+            isLoading: true,
+            isError: false
+        }
+    },
     methods: {
         pushUrl() {
             const { data } = this
@@ -25,6 +35,13 @@ export default {
             } else {
                 this.$router.push({ path: `/demo/${data.path}` })
             }
+        },
+        load() {
+            this.isLoading = false
+        },
+        error() {
+            this.isError = true
+            this.isLoading = false
         }
     }
 }
@@ -32,9 +49,12 @@ export default {
 
 <style lang="scss" scoped>
 @for $i from 1 through 30 {
+    $random1: random(255);
+    $random2: random(255);
+    $random3: random(255);
     .bg-color#{$i} {
         color: #fff;
-        background-image: linear-gradient(to right, rgba(random(255), random(255), random(255), 0.4), rgba(random(255), random(255), random(255), 0.4));
+        background: linear-gradient(to right, rgba($random1, $random2, $random3, 0.4), rgba($random1, $random3, $random2, 0.4));
     }
 }
 .card {
@@ -49,10 +69,63 @@ export default {
         height: 80%;
         overflow: hidden;
 
+        .placeholder-cover {
+            width: 50%;
+            height: auto;
+            transform: translateX(50%) translateY(25%);
+        }
+
+        .placeholder-cover1 {
+            display: block;
+            width: 100%;
+            height: auto;
+        }
+        .placeholder-cover2 {
+            display: none;
+            width: 100%;
+            height: auto;
+        }
+
+        &:hover {
+            .placeholder-cover1 {
+                display: none;
+            }
+            .placeholder-cover2 {
+                display: block;
+            }
+        }
+
         .cover {
             width: 100%;
             height: 100%;
             object-fit: contain;
+        }
+    }
+
+    .animated-bg {
+        $random1: random(255);
+        $random2: random(255);
+        $random3: random(255);
+        animation-duration: 3s;
+        animation-fill-mode: forwards;
+        animation-iteration-count: infinite;
+        animation-name: placeHolderShimmer;
+        animation-timing-function: linear;
+        background: linear-gradient(
+            90deg,
+            rgba($random1, $random2, $random3, 0.2) 10%,
+            rgba($random1, $random2, $random3, 0.1) 18%,
+            rgba($random1, $random2, $random3, 0.2) 33%
+        );
+        background-size: 600px 100px;
+    }
+
+    @keyframes placeHolderShimmer {
+        0% {
+            background-position: -600px 0;
+        }
+        100% {
+            background-position: 600px 0;
         }
     }
 
