22e65e700eeb21fc9317b8766bc7b3e800f01985
Snail-Lu <snaillu@yeah.net>
2022-06-13 18:18:40 +0800

    新增v-clipboard引用

--- src/config/index.js
+++ src/config/index.js
@@ -51,7 +51,7 @@ export default {
             title: 'CSS DEMO库',
             desc: '纯CSS实现的DEMO合集',
             coverUrl:
-                'https://ouch-cdn2.icons8.com/ljMlLxJIb3IVgWrhlk-dBnDMyYj05lRSWlgP4p4t0tI/rs:fit:256:256/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvNjYw/L2U2Y2VhNTNhLWQ4/OGUtNGYxNi1iMGJi/LWM3Zjg5MmQ4YzM3/YS5wbmc.png'
+                'https://ouch-cdn2.icons8.com/_bN6rv0CLIWcKM8fT4uafDU0ENGXI_dpSayR_2cm_L8/rs:fit:256:208/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvMTkx/LzRiMzM4M2VjLWM5/OWYtNDkzMS1hZTI0/LWRmYTg1YTk5N2Q2/Yy5wbmc.png'
         },
         {
             id: 12,
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import Clipboard from 'v-clipboard'
+
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 // 按需引入ElementUI组件
 import ElementUI from '@/components/ElementUI'
+import { Message } from 'element-ui'
 
 import '@/styles/index.scss' // global css
 
@@ -17,8 +20,10 @@ import uploader from 'vue-simple-uploader'
 
 Vue.use(ElementUI)
 Vue.use(uploader)
+Vue.use(Clipboard)
 
 Vue.prototype.req = request
+Vue.prototype.$message = Message
 
 Vue.config.productionTip = false
 
