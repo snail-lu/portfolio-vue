3dbd05489844b625c9c44ca5ac7c27b2bcc92281
Snail-Lu <snaillu@yeah.net>
2022-06-14 22:17:37 +0800

    update

--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -23,10 +23,11 @@ export default {
 
 .app-wrapper {
     @include clearfix;
-    // background-image: linear-gradient(-225deg, #cbbacc 0%, #2580b3 100%);
+    background-image: linear-gradient(-225deg, #cbbacc 0%, #2580b3 100%);
     // background-image: linear-gradient(60deg, #96deda 0%, #50c9c3 100%);
     // background-image: linear-gradient(to top, #4481eb 0%, #04befe 100%);
-    background-image: linear-gradient(-225deg, #69eacb 0%, #eaccf8 48%, #6654f1 100%);
+    // background-image: linear-gradient(-225deg, #69eacb 0%, #eaccf8 48%, #6654f1 100%);
+    // background-image: linear-gradient(-225deg, #2cd8d5 0%, #c5c1ff 100%);
     // background-color: #4b76cf;
     // background-color: #6abeef;
 }
