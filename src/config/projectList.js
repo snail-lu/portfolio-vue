aeac064c7da45718f184aa54ad304f05c983f8e3
Snail-Lu <snaillu@yeah.net>
2022-06-18 11:14:36 +0800

    demo及project配置拆分成不同文件

---
+++ src/config/projectList.js
@@ -0,0 +1,53 @@
+// 项目列表
+export default [
+    {
+        id: 1,
+        path: 'vue-management-system',
+        title: '商城后台管理系统',
+        reposity: 'https://github.com/Snail-Lu/vue-management-system',
+        url: 'https://snail-lu.github.io/vue-management-system',
+        desc: '基于Element-UI的商城后台管理系统项目',
+        coverUrl:
+            'https://ouch-cdn2.icons8.com/ecbhnQxhdJFde61dFLcKFAN0UHTVu5zKlS2EU1keAls/rs:fit:256:256/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNzUy/LzliZjFjOWFmLTU1/ZjItNDcyNy05MDhk/LTdlNDU2YTcwMDE1/Ni5zdmc.png'
+    },
+    {
+        id: 2,
+        path: 'vue-vant-mall',
+        title: '移动端H5商城',
+        reposity: 'https://github.com/Snail-Lu/vue-vant-mall',
+        url: 'https://snail-lu.github.io/vue-vant-mall',
+        desc: '基于vue-cli + vant创建的移动端商城项目',
+        coverUrl:
+            'https://ouch-cdn2.icons8.com/YB-zOIvPGvf-ffPo7lBspDJhDkM3cFGuEQnlMokjwfU/rs:fit:256:192/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvOTA4/LzZiMDVmOTIxLTNi/MWMtNDdlOC1hYmM1/LTRiZmE1ZTgxNzlj/Yy5zdmc.png'
+    },
+    {
+        id: 3,
+        path: 'uni-app-mall',
+        title: '微信小程序商城',
+        reposity: 'https://github.com/Snail-Lu/uni-app-mall',
+        url: '',
+        desc: '基于uni-app创建的小程序商城',
+        coverUrl:
+            'https://ouch-cdn2.icons8.com/fO8LMVpaqkMF7uIT9-YTgCRUH7t89bMK_nzO0vpbh-4/rs:fit:256:192/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNjI0/L2ZhYWMwYTY5LTY4/ZGEtNGRjMS1iMWJk/LTU5OGI1NzIyOGY0/NS5zdmc.png'
+    },
+    {
+        id: 4,
+        path: 'official-website',
+        title: '纯静态官网',
+        reposity: 'https://github.com/Snail-Lu/official-website',
+        url: 'https://snail-lu.github.io/official-website',
+        desc: '基于vue3开发的纯静态公司官网项目',
+        coverUrl:
+            'https://ouch-cdn2.icons8.com/dk6EC6P4hrMLRN3Z3IqcLeGqs0mMXDpwFxRbIC5V0ic/rs:fit:256:256/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNDk4/L2M2NjFiODM1LWIx/YTgtNGZjOC1iYzIz/LTE0OWE2M2VhNDNm/Mi5zdmc.png'
+    },
+    {
+        id: 5,
+        path: 'portfolio-react',
+        title: 'React 项目集',
+        reposity: 'https://github.com/Snail-Lu/portfolio-react',
+        url: 'https://portfolio-react-snail.vercel.app',
+        desc: '基于React开发的DEMO合集项目',
+        coverUrl:
+            'https://ouch-cdn2.icons8.com/IgD2F2hOXg9uBKCUFPKbyP6sp8K4AuCWKPOE0c-5Wag/rs:fit:256:256/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvOTg2/LzRjNTM3NDFhLTZi/MGUtNDE0OS1hMjU4/LTg5MjY3NzM4M2Q5/Yi5zdmc.png'
+    }
+]
