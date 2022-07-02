82dbf631149b4ac48862cb3ce01ec99f1bb64ee3
Snail-Lu <snaillu@yeah.net>
2022-07-02 16:38:46 +0800

    element-plus国际化

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
-    <div id="app">
-        <router-view />
-    </div>
+    <el-config-provider :locale="locale">
+        <div id="app">
+            <router-view />
+        </div>
+    </el-config-provider>
 </template>
 
 <script>
-export default {
-    name: 'App',
-    mounted() {},
-    methods: {}
-};
+import { defineComponent } from 'vue';
+import { ElConfigProvider } from 'element-plus';
+
+import zhCn from 'element-plus/lib/locale/lang/zh-cn';
+
+export default defineComponent({
+    components: {
+        ElConfigProvider
+    },
+    setup() {
+        return {
+            locale: zhCn
+        };
+    }
+});
 </script>
