778d183e31ec7346bc94aa53118de455aa555836
Snail-Lu <snaillu@yeah.net>
2022-07-11 20:27:37 +0800

    页面title格式修改

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,10 +1,10 @@
-import defaultSettings from '@/settings'
+import defaultSettings from '@/settings';
 
-const title = defaultSettings.title || ''
+const title = defaultSettings.title || '';
 
 export default function getPageTitle(pageTitle) {
-	if (pageTitle) {
-		return `${pageTitle} - ${title}`
-	}
-	return `${title}`
+    if (pageTitle) {
+        return `${title} | ${pageTitle}`;
+    }
+    return `${title}`;
 }
