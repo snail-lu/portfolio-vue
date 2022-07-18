81f77ca4ff20ebedd981e60a2c5278e93e70feb9
Snail-Lu <snaillu@yeah.net>
2022-07-18 22:08:56 +0800

    code format

--- src/views/demo/index.vue
+++ src/views/demo/index.vue
@@ -48,11 +48,9 @@ export default {
 @for $i from 0 through 30 {
     .bg-color#{$i} {
         color: #000;
-        background-image: linear-gradient(
-            random(360) + deg,
-            rgba(random(255), random(255), random(255), 0.5),
-            rgba(random(255), random(255), random(255), 0.5)
-        );
+        background-image: linear-gradient(random(360) + deg,
+                rgba(random(255), random(255), random(255), 0.5),
+                rgba(random(255), random(255), random(255), 0.5));
     }
 }
 
