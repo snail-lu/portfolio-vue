c3fa64632ee975159799c0efe60c1ac1446da9e3
Snail-Lu <snaillu@yeah.net>
2022-09-05 22:21:45 +0800

    Updates

---
+++ assets/index2.b52da701.js
@@ -0,0 +1,1 @@
+import{u as e,ag as l,ah as o}from"./index.4fbe083e.js";const i=["class","style"],E=/^on[A-Z]/,f=(a={})=>{const{excludeListeners:c=!1,excludeKeys:t}=a,u=e(()=>((t==null?void 0:t.value)||[]).concat(i)),s=o();return s?e(()=>{var n;return l(Object.entries((n=s.proxy)==null?void 0:n.$attrs).filter(([r])=>!u.value.includes(r)&&!(c&&E.test(r))))}):e(()=>({}))};export{f as u};
