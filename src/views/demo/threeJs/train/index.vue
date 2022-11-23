8b5d23121297b43a49ed933fcb87ce5f5515f630
Snail-Lu <snaillu@yeah.net>
2022-11-23 10:28:55 +0800

    feat: 更新火车模型‘

--- src/views/demo/threeJs/train/index.vue
+++ src/views/demo/threeJs/train/index.vue
@@ -62,7 +62,7 @@ export default {
         },
 
         stop() {
-            loop.stop;
+            loop.stop();
         }
     }
 };
--- src/views/demo/threeJs/train/systems/controls.js
+++ src/views/demo/threeJs/train/systems/controls.js
@@ -3,9 +3,18 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 function createControls(camera, canvas) {
     const controls = new OrbitControls(camera, canvas);
 
+    // 启用阻尼感
     controls.enableDamping = true;
 
     controls.target.y = 1;
+
+    // 启用自动旋转
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 1;
+
+    // 限制放大和缩小的距离
+    // controls.minDistance = 50;
+    // controls.maxDistance = 100;
 
     controls.tick = () => controls.update();
 
