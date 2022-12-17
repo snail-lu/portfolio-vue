5799dbfdaea245be0081ca262518d9dc9516561c
Snail-Lu <snaillu@yeah.net>
2022-12-17 22:00:29 +0800

    feat: 本地图片增加水印功能

--- src/utils/file.js
+++ src/utils/file.js
@@ -1,12 +1,12 @@
 // 根据图片URL获取图片文件
-export function imgUrlToFile(url, filename = 'a') {
+export function imgUrlToFile(url, watermarker, filename = 'a') {
     return new Promise(resolve => {
         const image = new Image();
         //如果图片url是网络url，要加下一句代码，解决跨域
         image.crossOrigin = "*";
         image.onload = function () {
-            const base64 = getBase64Image(image);
-            const imgFile = dataURLToFile(base64, filename);
+            const dataUrl = imgToDataURL(image, watermarker);
+            const imgFile = dataURLToFile(dataUrl, filename);
             resolve(imgFile)
         };
         image.src = url;
@@ -14,8 +14,8 @@ export function imgUrlToFile(url, filename = 'a') {
 
 }
 
-// 将url转换为base64
-export function getBase64Image(img) {
+// 将img经过canvas绘图后输出为dataURL
+export function imgToDataURL(img, watermarker = '我是一段水印') {
     const { width, height } = img;
     const canvas = document.createElement("canvas");
     canvas.width = width;
@@ -24,7 +24,7 @@ export function getBase64Image(img) {
     ctx.drawImage(img, 0, 0, width, height);
     ctx.fillStyle = 'red';
     ctx.font = '50px sans-serif';
-    ctx.fillText('我是一段水印文字', 100, 100);
+    ctx.fillText(watermarker, 100, 100);
     const ext = img.src.substring(img.src.lastIndexOf(".") + 1).toLowerCase();
     const type = `image/${ext}`
     const dataURL = canvas.toDataURL(type);
@@ -39,4 +39,17 @@ export function dataURLToFile(dataurl, filename) {
         u8arr[n] = bstr.charCodeAt(n);
     }
     return new File([u8arr], filename, { type: mime });
+}
+
+// file或blob转img标签
+export function fileOrBlobToImg(blob) {
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+        reader.addEventListener('load', () => {
+            let img = new Image();
+            img.src = reader.result;
+            img.addEventListener('load', () => resolve(img));
+        });
+        reader.readAsDataURL(blob);
+    });
 }
--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -19,7 +19,7 @@
             </el-form-item>
             <el-form-item label="本地图片：" v-if="form.type == 2">
                 <el-upload class="avatar-uploader" action="#" :auto-upload="false" :on-change="onUploadFile" :show-file-list="false">
-                    <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+                    <el-image v-if="imageUrl" :src="imageUrl" class="avatar" fit="contain" />
                     <el-icon v-else class="avatar-uploader-icon"><i-ep-plus /></el-icon>
                 </el-upload>
             </el-form-item>
@@ -34,7 +34,7 @@
 <script setup>
 import { getCurrentInstance, reactive, ref } from 'vue';
 import { getNowTime } from '@/utils/date';
-import { imgUrlToFile } from '@/utils/file';
+import { imgUrlToFile, fileOrBlobToImg, imgToDataURL, dataURLToFile } from '@/utils/file';
 
 const instance = getCurrentInstance();
 
@@ -42,22 +42,31 @@ const form = reactive({
     text: getNowTime(),
     imgUrl: 'https://t7.baidu.com/it/u=2168645659,3174029352&fm=193&f=GIF',
     transformedImgUrl: '',
-    type: 1
+    type: 2
 });
 
 // 预览列表
 const previewList = ref([]);
 
-function onUploadFile(file, list) {
+const imageUrl = ref('');
+// 本地图片上传时添加水印
+async function onUploadFile(file) {
     console.log(file, 'upload file');
-    // addWaterMarker(file);
+    imageUrl.value = URL.createObjectURL(file.raw);
+    // 上传的文件转换为img标签
+    let image = await fileOrBlobToImg(file.raw);
+    const dataUrl = imgToDataURL(image, form.text);
+    const imgFile = dataURLToFile(dataUrl, 'a');
+    const imgUrl = URL.createObjectURL(imgFile);
+    form.transformedImgUrl = imgUrl;
+    previewList.value = [imgUrl];
 }
-const data = ref([]);
 
+// 网络图片添加水印
 async function add() {
     if (form.imgUrl) {
-        // 将url转换得到file文件
-        const imgFile = await imgUrlToFile(form.imgUrl);
+        // 将url图片添加水印后再输出为file文件
+        const imgFile = await imgUrlToFile(form.imgUrl, form.text);
         // 将文件转换为可以展示的URL
         const imgUrl = URL.createObjectURL(imgFile);
         form.transformedImgUrl = imgUrl;
@@ -65,83 +74,6 @@ async function add() {
     } else {
         instance.proxy.$message.error('请输入网络图片地址');
     }
-}
-/**
- * 添加水印
- */
-async function addWaterMarker(file, el = '#markImg') {
-    // 将文件blob转换成图片
-    let img = await blobToImg(file);
-    return new Promise(async (resolve, reject) => {
-        try {
-            // 创建canvas画布
-            let canvas = document.createElement('canvas');
-            //等比例调整canvas宽高，以缩小图片体积
-            let imgRatio = img.naturalWidth / img.naturalHeight; //图片比例
-            canvas.width = 750; //默认设置成750
-            canvas.height = canvas.width / imgRatio;
-
-            let ctx = canvas.getContext('2d');
-
-            // 填充上传的图片
-            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-            // 生成水印图片
-            const markWidth = document.querySelector(el).clientWidth;
-            let zoom = (canvas.width * 0.3) / markWidth;
-            let markEle = document.querySelector(el);
-            // 先缩放水印html再转成图片
-            markEle.style.transform = `scale(${zoom})`;
-            const markImg = await htmlToCanvas(markEle);
-
-            // 填充水印
-            ctx.drawImage(
-                markImg,
-                canvas.width - markImg.width - 15 * zoom,
-                canvas.height - markImg.height - 15 * zoom,
-                markImg.width,
-                markImg.height
-            );
-
-            // 将canvas转换成blob
-            canvas.toBlob((blob) => resolve(blob));
-        } catch (error) {
-            reject(error);
-        }
-    });
-}
-
-/**
- * blob转img标签
- */
-function blobToImg(blob) {
-    return new Promise((resolve, reject) => {
-        let reader = new FileReader();
-        reader.addEventListener('load', () => {
-            let img = new Image();
-            img.src = reader.result;
-            img.addEventListener('load', () => resolve(img));
-        });
-        reader.readAsDataURL(blob);
-    });
-}
-
-/**
- * html转成canvas，需要安装html2canvas.js插件
- */
-function htmlToCanvas(el, backgroundColor = 'rgba(0,0,0,.1)') {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const markImg = await html2canvas(el, {
-                allowTaint: false, //允许污染
-                useCORS: true,
-                backgroundColor //'transparent'  //背景色
-            });
-            resolve(markImg);
-        } catch (error) {
-            reject(error);
-        }
-    });
 }
 </script>
 
@@ -167,7 +99,8 @@ function htmlToCanvas(el, backgroundColor = 'rgba(0,0,0,.1)') {
     }
 }
 
-.el-icon.avatar-uploader-icon {
+.el-icon.avatar-uploader-icon,
+.avatar {
     font-size: 28px;
     color: #8c939d;
     width: 178px;
