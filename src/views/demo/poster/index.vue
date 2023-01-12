c26bff5e77312e9284fd33ca9d1583d8ab0f5325
Snail-Lu <snaillu@yeah.net>
2023-01-12 22:37:34 +0800

    feat: update poster

--- src/views/demo/poster/index.vue
+++ src/views/demo/poster/index.vue
@@ -2,13 +2,11 @@
     <Demo :data="demoInfo">
         <!-- <canvas ref="posterCanvas"></canvas> -->
         <div class="poster-html" ref="posterRef">
-            <img
-                class="poster-img"
-                src="https://s3.ap-southeast-1.amazonaws.com/imghweshop.hla.com/edm/image/2021-05/dc10ae5368d5437099360db6d4d6eefa.jpg"
-                alt=""
-                @load="imgLoad()"
-                crossorigin="anonymous"
-            />
+            <div class="share-info">
+                <img src="../../../assets/icons/logo.png" alt="" class="logo" />
+                <div>Snail</div>
+            </div>
+            <img class="poster-img" src="https://s1.ax1x.com/2023/01/12/pSujg2T.jpg" alt="" @load="imgLoad()" />
             <div class="goods-info">
                 <p class="title">{{ goods.goodsName }}</p>
                 <p class="price">
@@ -18,13 +16,7 @@
             </div>
             <div class="poster-info">
                 <vue-qr class="qr-img" :text="goods.url"></vue-qr>
-                <ul>
-                    <li>Recommend to you by Snail</li>
-                    <li>Code: {{ goods.goodsCode }}</li>
-                    <li>Official E-mall Sale</li>
-                    <li>Scan to shop in E-Mall</li>
-                </ul>
-                <img src="../../../assets/icons/logo.png" alt="" class="logo" />
+                <div>扫码立即购买</div>
             </div>
         </div>
         <!-- <div class="poster-success" v-show="!isPoster">
@@ -38,7 +30,7 @@ import Demo from '@/components/Demo/index.vue';
 import vueQr from 'vue-qr/src/packages/vue-qr.vue';
 import { onMounted, ref } from 'vue';
 const demoInfo = {
-    title: '商品海报',
+    title: '',
     url: ''
 };
 
@@ -74,6 +66,16 @@ const imgLoad = () => {
     background-color: #fff;
     margin: 0 auto;
 
+    .share-info {
+        display: flex;
+        align-items: center;
+        .logo {
+            width: 50px;
+            height: 50px;
+            margin-right: 20px;
+        }
+    }
+
     .poster-img {
         width: 100%;
         height: auto;
@@ -81,14 +83,12 @@ const imgLoad = () => {
     }
 
     .goods-info {
-        // font-family: 'fzlthjw';
         font-size: 15px;
         color: #040000;
         display: flex;
         align-items: center;
         justify-content: space-between;
         padding: 10px 15px 0;
-        margin-bottom: -40px;
         .title {
             margin-right: 60px;
         }
@@ -115,42 +115,19 @@ const imgLoad = () => {
             }
         }
     }
-}
-.poster-info {
-    display: flex;
-    align-items: center;
-    padding: 70px 79px;
-    font-size: 20px;
-    color: #3e3a39;
-    position: relative;
-    .logo {
-        width: 117px;
-        height: auto;
-        position: absolute;
-        right: 79px;
-        top: 50%;
-        transform: translateY(-50%);
+
+    .poster-info {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        color: #3e3a39;
+        position: relative;
+        .qr-img {
+            width: 98px;
+            height: 98px;
+        }
     }
-    ul {
-        margin-left: 29px;
-    }
-    li {
-        line-height: 2em;
-    }
-}
-.qr-img {
-    width: 156px;
-    height: 156px;
-}
-.popup-btn {
-    display: block;
-    font-family: 'fzlthjw';
-    width: 380px;
-    height: 96px;
-    padding: 0;
-    background-color: #cd2220;
-    margin: 0 auto;
-    color: #fff;
 }
 .poster-success {
     // display: flex;
